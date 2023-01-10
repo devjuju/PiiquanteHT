ea2cee71358fd6ebbf9ce05420e97ea123568065
devjuju <jleleubellpro1994@gmail.com>
2023-01-10 15:36:15 +0100

    Étape 4 : Construire la route Sauce de l’API

---
+++ middleware/auth.js
@@ -0,0 +1,20 @@
+// Étape 3 : Démarrer le middleware
+// Ajout d’authorize pour la validation des tokens.
+// SOURCE (cours) : Passez au Full Stack avec Node.js, Express et MongoDB
+// Configurez le middleware d'authentification
+// Implémentez le middleware d'authentification
+const jwt = require('jsonwebtoken');
+ 
+module.exports = (req, res, next) => {
+   try {
+       const token = req.headers.authorization.split(' ')[1];
+       const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
+       const userId = decodedToken.userId;
+       req.auth = {
+           userId: userId
+       };
+	next();
+   } catch(error) {
+       res.status(401).json({ error });
+   }
+};
